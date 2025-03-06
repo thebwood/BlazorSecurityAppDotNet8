1035642e2e9cfd6200979c736ad312bf60c05796
Brian David Wood <brian_d_wood@yahoo.com>
2025-03-06 00:02:08 -0500

    Set up bootstrap layout

---
+++ BlazorSecurityApp/BlazorSecurityApp.Web/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorSecurityApp.Web.Components.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        [Inject]
+        public AuthenticationStateProvider authStateProvider { get; set; } = default!;
+
+        [Inject]
+        public NavigationManager navManager { get; set; } = default!;
+
+
+    }
+}
